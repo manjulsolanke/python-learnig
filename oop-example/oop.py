ba8276c4d6f0c9199adaedeab30a2362d774a9d7
manjuldas.solanke <manjuldas.solanke@externals.hilti.com>
2021-04-02 17:33:39 +0530

    restructure and add oops concepts examples

---
+++ oop-example/oop.py
@@ -0,0 +1,7 @@
+class Test:  # Define a class
+    def m1(self): # declare the method
+        print('Python is oop programming language')
+        print('Python is oop programming language')
+        print('Python is oop programming language')
+t=Test() # create  a object 
+t.m1() # call the method
