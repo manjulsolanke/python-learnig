1b23c598876f3b870e6a0eb3aabe0fdcdf34089b
manjuldas.solanke <manjuldas.solanke@externals.hilti.com>
2021-04-02 17:20:04 +0530

    functionl PL examples

---
+++ conditions/functional-pl-examples/f-1.py
@@ -0,0 +1,6 @@
+def f1():
+    print('Python is functional programming language')
+    print('Python is functional programming language')
+    print('Python is functional programming language')
+    print('Python is functional programming language')
+f1()
