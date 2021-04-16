1dec3d27d247c91abf9fc0dfdad366e506a8827b
Manjul <solankemn@gmail.com>
2021-04-16 05:13:06 +0000

    added unicode examples

---
+++ operator/relational_operator/uni-relational.py
@@ -0,0 +1,28 @@
+# unicode comparison for alphabets 
+s1 = 'foo'
+s2 = 'baar'
+print(s1 <  s2)
+print(s1 <= s2)
+print(s1 >  s2)
+print(s1 >= s2)
+
+
+# unicode comparison of same name
+s1 = 'foo'
+s2 = 'foo'
+print(s1 <  s2)
+print(s1 <= s2)
+print(s1 >  s2)
+print(s1 >= s2)
+
+
+# unicode comaprison of capitla nad small alphabets
+s1 = 'foo'
+s2 = 'Foo'
+print("Unicode value of samll f is:", ord('f'))
+print("Unicodegit  value of capital F is:", ord('F'))
+print(s1 <  s2)
+print(s1 <= s2)
+print(s1 >  s2)
+print(s1 >= s2)
+
---
+++ operator/relational_operator/unicode.py
@@ -0,0 +1,4 @@
+print("unicode value of a is  :",ord('a'))
+print("unicode value of A is  :",ord('A'))
+print("unicode value of 97 is :",chr(97))
+print("unicode value of 67 is :",chr(67))
