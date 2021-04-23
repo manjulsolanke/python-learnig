17a8c006d1d8951036c4d5e157b4e5628e536876
Manjul <solankemn@gmail.com>
2021-04-23 04:39:47 +0000

    added eval and command line argument functions

---
+++ input_output_statement/command_line_arguments/cmd_line_arguments.py
@@ -0,0 +1,7 @@
+from sys import argv
+print(type(argv))
+print(argv[0]) # name of file
+print(argv[1]) # first argument
+print(argv[2]) # second argument
+print(argv[3]) # third argument
+print(argv)
