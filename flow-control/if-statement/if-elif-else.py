d06830fec108cee3e0bbaa95af8a11bcf9f16983
Manjul <solankemn@gmail.com>
2021-04-29 02:53:01 +0000

    flow chart

---
+++ flow-control/if-statement/if-elif-else.py
@@ -0,0 +1,12 @@
+# else block is optional
+sub = input('Enter your favourite sub:')
+if sub == 'c':
+    print('c langauage is base of all')
+elif sub == 'java':
+    print('java is very popular language')
+elif sub == 'python':
+    print('one the best lanuage')
+elif sub == 'networking':
+    print('everything depends on networking')
+else:
+    print('all sub are good for me!')
---
+++ flow-control/if-statement/if-else.py
@@ -0,0 +1,6 @@
+name = input('Enter name:')
+if name == 'foo':
+    print('hello foo')
+else:
+    print('Hello There')
+print('How are you?')
