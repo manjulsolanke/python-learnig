9f0bdcc6a6b0f360a43bc737d77a03f01f902ce6
Manjul Solanke <solankemn@gmail.com>
2021-06-16 08:28:58 +0530

    slice operator

--- string/slice.py
+++ string/slice.py
@@ -1,7 +1,8 @@
-string ='Manjuldas'
+string ='abcdefghij'
 print(string[0:3])
-print(string[1:3:5])
-print(string[-1:-4:-5])
+print(string[1::2]) # print character after 2 iteration
+print(string[-1:-9:-2])
 print(string[:5]) # 0 is default value and its being value
 print(string[0:]) # if end is not mentioned its will take all value
 print(string[:]) # total value going to display
+print(string[::2])
