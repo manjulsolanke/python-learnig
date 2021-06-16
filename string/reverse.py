fcd3568943cef8707ddb54145bf7c3517cb0b116
Manjul Solanke <solankemn@gmail.com>
2021-06-16 10:18:03 +0530

    slice operator examples

---
+++ string/reverse.py
@@ -0,0 +1,4 @@
+# print the string in reverse using slice operator
+s = 'abcdfghijk'
+s[::-1]
+print(s[::-1])
--- string/slice.py
+++ string/slice.py
@@ -1,6 +1,12 @@
+#[being:end:steps] Steps values can be positive or negative value
+# If steps value is positive then
+  # left to right (Forward direction) - Begin to end - 1
+# if steps value is negitive then
+  # right to left (Backward direction)  - Begin to end + 1
 string ='abcdefghij'
 print(string[0:3])
 print(string[1::2]) # print character after 2 iteration
+#print(string[-1:-9:-0]) #slice step cannot be zero
 print(string[-1:-9:-2])
 print(string[:5]) # 0 is default value and its being value
 print(string[0:]) # if end is not mentioned its will take all value
