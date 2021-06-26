a74e200531ecc429022ea84fd446234ad65f452b
Manjul Solanke <solankemn@gmail.com>
2021-06-26 21:57:44 +0530

    added find function

---
+++ string/finding_sub_string.py
@@ -0,0 +1,12 @@
+# WAP to find the first  occurrence of sub string in a main string
+# mainstring.find(substring, beginnig, ending)
+mainstr = input('Enter main string: ')
+substr = input('Enter sub string:  ')
+
+# find position of sub in mainstr
+# search from 0th to last characters in mainstr
+n = mainstr.find(substr, 0, len(mainstr))
+if n == -1:
+    print('Sub string not found')
+else:
+    print('sub string found at position: ', n+1)
--- string/remove_space.py
+++ string/remove_space.py
@@ -1,16 +1,9 @@
 # WAP for removing the space in string
-#FName = input("Enter the first name")
-#LName = input("Enter the last name")
-
-
 # this is problems statement
 if 'Manjul '=='Manjul': # space could lead to wrong result
     print('Welcome')
 else:
     print('Name is not found')
-    
-
-
 name = '   Manjul Solanke   '
 fname = 'Manjul Solanke'
 print(name.lstrip()) # remove space from left side
