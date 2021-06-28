0877ab23e82cba7e77a5759f7c7c0ac2bdcf1fb8
Manjul Solanke <solankemn@gmail.com>
2021-06-28 08:51:14 +0530

    appends repeation program

--- string/repeating_string.py
+++ string/repeating_string.py
@@ -1,2 +1,4 @@
 str = 'Core python'
 print(str*5) # multiplication operator with string.
+print(5*str) # order donesn't matters here, but make sure one argunent should be string and one argument should be int.
+
