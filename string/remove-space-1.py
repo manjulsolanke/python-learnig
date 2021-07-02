a0302ba1f6f5bcf7278e3f99035c6ec1eeb23364
Manjul Solanke <solankemn@gmail.com>
2021-07-02 16:58:01 +0530

    compare operator

---
+++ string/remove-space-1.py
@@ -0,0 +1,9 @@
+city = input('Enter the our city-name:')
+if city == 'Pune':
+    print('Namskar Punekar')
+elif city == 'Cheenai':
+    print('Vannakam')
+elif city == 'Bangalore':
+    print('Weclome to TechHalli')
+elif city == 'Parbhani':
+    print('Ram Ram Parbhanikar')
