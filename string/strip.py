14781e78100d61adeeb26acff8c8923935bb1bad
Manjul Solanke <solankemn@gmail.com>
2021-07-09 14:49:03 +0530

    strip examples

---
+++ string/strip.py
@@ -0,0 +1,13 @@
+CITY = input("Please enter the CITY name: ")
+#sCITY = CITY.strip()
+if CITY == 'Parbhani':
+    print('Jagat Germany ani Bharatat Parbhani')
+elif CITY == 'Pune':
+    print('Pune tithe kya Pune')
+elif CITY == 'Bangalore':
+    print('Welcome to TechHalli')
+elif CITY == 'Chennai':
+    print('Wannacame Anna')
+else:
+    print('Enter the valid CITY')
+    
