106ef02ee8acaac5173c6fb7a1b1e6b107e268d3
Manjul Solanke <solankemn@gmail.com>
2022-11-22 07:49:39 +0530

    added string example

---
+++ string/string-replicement.py
@@ -0,0 +1,11 @@
+age = 24
+print("My age is  " +  str(age) + " years")
+print("My age is {0} years ".format(age))
+
+days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
+print(days[::5])
+
+data = "1:A, 2:B, 3:C, 4:D, 5:E, 6:F, 7:G, 8:H"
+print(data[:-1:5])
+
+4
