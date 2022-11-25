32bde8d6e99af18123947c03e81ea047a95bdb87
Manjul Solanke <solankemn@gmail.com>
2022-11-25 08:17:54 +0530

    slice and len program

--- slice/slice-uppercase.py
+++ slice/slice-uppercase.py
@@ -1,8 +1,17 @@
 '''
 uppercase and lenght 
 '''
-s = "manjul"
+s = "manjulsolanke"
 print("print the lenth of s:",len(s))
-print(s[0:len(s) -1])
-output = s[0].upper() + s[1:len(s)]
+print(s[0:len(s)]) 
+# print first charactor in upper case
+print("print first charactor in upper case")
+output = s[0:len(s)-1]+s[-1].upper()
+print(output)
+
+# print first and last character in upper case
+
+
+print("print first and last character in upper case")
+output = s[0].upper() + s[1:len(s) -1] + s[-1].upper()
 print(output)
